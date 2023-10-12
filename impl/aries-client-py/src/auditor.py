282c030b886da8817d553b1af698836b4da02f5d
Armin Veres <armin.veres@hotmail.com>
2023-10-12 12:51:59 +0200

    fix(client): missing flag from run script
    multitenant flag was not read

--- impl/aries-client-py/src/auditor.py
+++ impl/aries-client-py/src/auditor.py
@@ -2,12 +2,32 @@
 Demonstrative auditor to mark soft- or firmware as vulnerable
 """
 # TODO: (aver)
-# - add http client
 # - add analysis of DB and post revocation
+
+from aiohttp import (
+    web,
+    ClientSession,
+    ClientRequest,
+    ClientResponse,
+    ClientError,
+    ClientTimeout,
+)
+
+
+class Auditor:
+    """
+    Class that holds logic for a sample auditor, marking software as vulnerable.
+    Works by querying the orion database and notifying the issuer, i.e., the admin of a
+    vulnerability.
+    """
+
+    def __init__(self):
+        self.client_session = ClientSession()
 
 
 def main():
-    pass
+    auditor = Auditor()
+    print("hello there")
 
 
 if __name__ == "__main__":
