dc2dbcecf79b21a6d7be7665722cb6c9665b4921
Armin Veres <armin.veres@hotmail.com>
2023-10-23 13:14:57 +0200

    feat: working multitenance
    Still lots todo:
    - decide on mechanism of how issuer is notified of onboarding

--- impl/src/agents/node.py
+++ impl/src/agents/node.py
@@ -1,14 +1,15 @@
 import asyncio
-import os
-
 import json
+import os
+import sys
 
 from agents.agent_container import (  # noqa:E402
+    AgentContainer,
     AriesAgent,
     arg_parser,
     create_agent_with_args,
 )
-from support.utils import log_json
+from support.utils import log_msg, prompt, prompt_loop
 
 
 # class NodeAgent:
@@ -29,6 +30,9 @@ class NodeAgent(AriesAgent):
         # based on cred_def_id
         self.cred_attrs = {}
 
+    # =============================================================================================
+    # Webhook handler implementations
+    # =============================================================================================
     async def handle_invitation(self, message):
         """
         Handle invitation received for connections
@@ -43,12 +47,36 @@ class NodeAgent(AriesAgent):
         # TODO: (aver) handle update
         pass
 
+    # =============================================================================================
+    # Additional methods
+    # =============================================================================================
+
+
+async def register_subnode(agent_container: AgentContainer, node_name: str):
+    """
+    Registers a subnode with name `node_name` to the agent in the `agent_container`
+    TODO: (aver) could move this to AgentContainer implementation
+    """
+    await agent_container.agent.register_or_switch_wallet(
+        target_wallet_name=f"{agent_container.ident}.sub.{node_name}",
+        public_did=agent_container.public_did
+        and (
+            (not agent_container.endorser_role)
+            or (not agent_container.endorser_role == "author")
+        ),
+        webhook_port=None,
+        mediator_agent=agent_container.mediator_agent,
+        endorser_agent=agent_container.endorser_agent,
+        taa_accept=agent_container.taa_accept,
+    )
+
 
 async def main(args):
     # First setup all the agent related stuff
     agent_container = await create_agent_with_args(args)
     # agent_container.seed = "Node1_00000000000000000000000000"
     # TODO: (aver) save seed for provisioning to pick up on
+    agent_container.seed = "d_000000000000000000000000752220"
 
     try:
         agent = NodeAgent(
@@ -73,9 +101,46 @@ async def main(args):
         )
         await agent_container.initialize(the_agent=agent)
 
-        while True:
-            # pass
-            await asyncio.sleep(0.1)
+        # =========================================================================================
+        # Event Loop
+        # =========================================================================================
+        # New prompt based event loop, events such as webhooks still run in the background.
+
+        # Setup options and make them dicts, so that they can be changed at runtime, although watch
+        # out for duplicated keys.
+        # TODO: (aver) find better way to interact with runtime options
+        options = {
+            "exit": "  [x]: Exit\n",
+        }
+
+        if agent_container.multitenant:
+            options["reg_subnode"] = "  [1]: Register a subnode, i.e., and edge node\n"
+
+        def get_prompt():
+            options.update
+            options_str = "Options:\n"
+            for key, value in list(options.items()):
+                options_str += value
+            options_str += "> "
+            return options_str
+
+        async for option in prompt_loop(get_prompt):
+            if option is not None:
+                option.strip()
+            if option is None or option == "":
+                log_msg("Please give an option")
+
+            if option == "1" and agent_container.multitenant:
+                edge_node_name = (
+                    await prompt("Enter a name for the subnode: ")
+                ).strip()
+                await register_subnode(agent_container, edge_node_name)
+
+            elif option in "xX":
+                sys.exit(0)
+
+            else:
+                log_msg("Unknown option: " + option)
 
     finally:
         await agent_container.terminate()
