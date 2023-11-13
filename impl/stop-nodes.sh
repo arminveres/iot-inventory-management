f146561c1462f1bb56078a59f697ab445aed7416
Armin Veres <armin.veres@hotmail.com>
2023-11-13 22:08:48 +0100

    feat(build): attachable containers + test setup for many nodes

--- impl/stop-nodes.sh
+++ impl/stop-nodes.sh
@@ -3,12 +3,14 @@ set -e
 
 NAME_PREFIX="controller_node_"
 DOCKER_CONTAINERS=$(docker container ls --format '{{.ID}} {{.Names}}' | grep $NAME_PREFIX)
-# CONTAINER_ID=$(echo "$DOCKER_CONTAINERS" | awk '{print $1}')
-# CONTAINER_NAME=$(echo "$DOCKER_CONTAINERS" | awk '{print $2}')
 
 while read -r line; do
-    CONTAINER_ID=$(echo "$line" | awk '{print $1}')
-    CONTAINER_NAME=$(echo "$line" | awk '{print $2}')
-    echo "Stopping $CONTAINER_NAME ..."
-    docker container stop "$CONTAINER_ID"
+    (
+        CONTAINER_ID=$(echo "$line" | awk '{print $1}')
+        CONTAINER_NAME=$(echo "$line" | awk '{print $2}')
+        echo "Stopping $CONTAINER_NAME ..."
+        docker container stop "$CONTAINER_ID" &>/dev/null
+    ) &
 done <<<"$DOCKER_CONTAINERS"
+
+rm -f ./.agent_cache/mass_onboarding
