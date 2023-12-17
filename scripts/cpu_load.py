c0a6440eee88111f753a5d913355d5a786e9ddec
Armin Veres <armin.veres@hotmail.com>
2023-12-17 21:36:20 +0100

    feat(scripts): add cpu load script for pimoromi blinkt

---
+++ scripts/cpu_load.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Courtesy of https://github.com/pimoroni/blinkt/blob/master/examples/cpu_load.py
+
+import time
+from sys import exit
+
+try:
+    import psutil
+except ImportError:
+    exit('This script requires the psutil module\nInstall with: sudo apt install python3-psutil')
+
+import blinkt
+
+blinkt.set_clear_on_exit()
+
+
+def show_graph(v, r, g, b):
+    v *= blinkt.NUM_PIXELS
+    for x in range(blinkt.NUM_PIXELS):
+        if v < 0:
+            r, g, b = 0, 0, 0
+        else:
+            r, g, b = [int(min(v, 1.0) * c) for c in [r, g, b]]
+        blinkt.set_pixel(x, r, g, b)
+        v -= 1
+
+    blinkt.show()
+
+
+blinkt.set_brightness(0.1)
+
+while True:
+    v = psutil.cpu_percent() / 100.0
+    show_graph(v, 255, 255, 255)
+    time.sleep(0.01)
